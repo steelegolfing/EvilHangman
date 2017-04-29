e3e92509b03c2c69d5c0bee0d3d55e7c98f91a3f
abbyantrich <abbyantrich@gmail.com>
2017-04-29 14:12:00 -0500

    Update buildHangman.h

--- buildHangman.h
+++ buildHangman.h
@@ -1,3 +1,5 @@
+
+
 #ifndef buildHangman_h
 #define buildHangman_h
 #include "renderLetters.h"
@@ -6,8 +8,15 @@
 #include "wordStruct.h"
 using namespace std;
 
-//draw gallows
+/*
+ * function: drawGallows
+ * description: draws the gallows to hang the hangman
+ * precondition: an instance of the plotter class exists
+ * postcondition: the gallows have been rendered
+ * return: void
+ */
 void drawGallows(){
+    //build gallows
     SDL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
@@ -15,20 +24,34 @@ void drawGallows(){
     SDL_RenderDrawLine();
 }
 
-//draw head
+/*
+ * function: drawHead
+ * description: draws the head of the hangman
+ * precondition: an instance of the plotter class exists
+ * postcondition: the hangman's head is rendered
+ * return: void
+ */
 void drawHead(){
+    //draw major part of head
     SDL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
-    //topper
+    //draw top piece of head
     SDL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
 }
-//draw body
+
+/*
+ * function: drawBody
+ * description: draws the body and head of the hangman
+ * precondition: an instance of the plotter class exists
+ * postcondition: the hangman's body is rendered along with the head
+ * return: void
+ */
 void drawBody(){
     //trapezoid body shape
     SDL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
@@ -36,48 +59,85 @@ void drawBody(){
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
+    //draws rest of body parts
     drawHead();
 }
-//draw larm
+
+/*
+ * function: drawLArm
+ * description: draws the hangman's left arm, along with the body and head
+ * precondition: an instance of the plotter class exists
+ * postcondition: the hangman's left arm is rendered along with the body and head
+ * return: void
+ */
 void drawLArm(){
+    //draw left arm
     SDL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
+    //draws rest of body parts
     drawBody();
     drawHead();
 }
-//draw rarm
+
+/*
+ * function: drawRArm
+ * description: draws the hangman's right arm, along with the body, head, and left arm
+ * precondition: an instance of the plotter class exists
+ * postcondition: the hangman's right arm is rendered along with the body, head, and left arm
+ * return: void
+ */
 void drawRArm(){
+    //draw right arm
     SDL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
+    //draws rest of body parts
     drawLArm():
     drawBody();
     drawHead();
 }
-//draw lleg
+
+/*
+ * function: drawLLeg
+ * description: draws the hangman's left leg, along with the rest of the body parts
+ * precondition: an instance of the plotter class exists
+ * postcondition: the hangman's left leg is rendered along with the rest of the body parts
+ * return: void
+ */
 void drawLLeg(){
+    //draw left leg
     SDL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
+    //draws rest of body parts
     drawRArm();
     drawLArm();
     drawBody();
     drawHead();
 }
-//draw rleg
+
+/*
+ * function: drawRLeg
+ * description: draws the hangman's right leg, along with the rest of the body parts
+ * precondition: an instance of the plotter class exists
+ * postcondition: the hangman's right leg is rendered along with the rest of the body parts
+ * return: void
+ */
 void drawRLeg(){
+    //draw right leg
     SDL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
+    //draws rest of body parts
     drawLLeg();
     drawRArm();
     drawLArm();
@@ -85,8 +145,15 @@ void drawRLeg(){
     drawHead();
 }
 
-//draw face
+/*
+ * function: drawFace
+ * description: draws the hangman's face, along with the rest of the body parts
+ * precondition: an instance of the plotter class exists
+ * postcondition: the hangman's face is rendered along with the rest of the body parts
+ * return: void
+ */
 void drawFace(){
+    //draw eyes
     drawX();
     drawX();
     //draw mouth
@@ -94,7 +161,7 @@ void drawFace(){
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
-    
+    //draws rest of body parts
     drawRLeg():
     drawLLeg();
     drawRArm();
@@ -104,12 +171,20 @@ void drawFace(){
     drawHead();
 }
 
-//draw lhand
+/*
+ * function: drawLHand
+ * description: draws the hangman's left hand, along with the rest of the body parts
+ * precondition: an instance of the plotter class exists
+ * postcondition: the hangman's left hand is rendered along with the rest of the body parts
+ * return: void
+ */
 void drawLHand(){
+    //draw left hand
     DL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
+    //draws rest of body parts
     drawFace();
     drawRLeg():
     drawLLeg();
@@ -118,13 +193,20 @@ void drawLHand(){
     drawBody();
     drawHead();
 }
-
-//draw rhand
+/*
+ * function: drawRHand
+ * description: draws the hangman's right hand, along with the rest of the body parts
+ * precondition: an instance of the plotter class exists
+ * postcondition: the hangman's right hand is rendered along with the rest of the body parts
+ * return: void
+ */
 void drawRHand(){
+    //draw right hand
     DL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
+    //draws rest of body parts
     drawLHand();
     drawFace();
     drawRLeg():
@@ -134,14 +216,21 @@ void drawRHand(){
     drawBody();
     drawHead();
 }
-
-//draw lfoot
+/*
+ * function: drawLFoot
+ * description: draws the hangman's left foot, along with the rest of the body parts
+ * precondition: an instance of the plotter class exists
+ * postcondition: the hangman's left foot is rendered along with the rest of the body parts
+ * return: void
+ */
 void drawLFoot(){
+    //draw left foot
     DL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
+    //draws rest of body parts
     drawRHand();
     drawLHand();
     drawFace();
@@ -153,13 +242,21 @@ void drawLFoot(){
     drawHead();
 }
 
-//draw rfoot
+/*
+ * function: drawRFoot
+ * description: draws the hangman's right foot, along with the rest of the body parts
+ * precondition: an instance of the plotter class exists
+ * postcondition: the hangman's right foot is rendered along with the rest of the body parts
+ * return: void
+ */
 void drawRFoot(){
+    //draw right foot
     DL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
+    //draws rest of body parts
     drawLFoot();
     drawRHand();
     drawLHand();
@@ -172,8 +269,15 @@ void drawRFoot(){
     drawHead();
 }
 
-//draw tie
+/*
+ * function: drawTie
+ * description: draws hangman's tie, along with the rest of the body parts
+ * precondition: an instance of the plotter class exists
+ * postcondition: hangman's tie is rendered along with the rest of the body parts
+ * return: void
+ */
 void drawTie(){
+    //draw hangmans bowtie
     DL_SetRenderDrawColor(gRender, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
@@ -181,6 +285,7 @@ void drawTie(){
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
     SDL_RenderDrawLine();
+    //draws rest of body parts
     drawRFoot();
     drawLFoot();
     drawRHand();
