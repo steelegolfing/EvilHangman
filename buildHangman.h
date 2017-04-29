0407422d6258375c545eecf5a69efb226babdf14
steelegolfing <london_steele@baylor.edu>
2017-04-29 12:13:17 -0500

    Added hangman class

---
+++ buildHangman.h
@@ -0,0 +1,9 @@
+#ifndef buildHangman_h
+#define buildHangman_h
+#include "renderLetters.h"
+#include "SDL_Plotter.h"
+#include "Single_Linked_List.h"
+#include "wordStruct.h"
+using namespace std;
+
+#endif /* buildHangman_h */
